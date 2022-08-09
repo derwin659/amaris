94f74890075c7eb8026ebce272fc28ae7d2b8c71
Derwin Nieves Martinez <dnieves@nisum.com>
2022-08-09 13:37:03 -0400

    primer commit

---
+++ src/main/java/com/amarispersona/amarispersona/AmarispersonaApplication.java
@@ -0,0 +1,13 @@
+package com.amarispersona.amarispersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarispersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarispersonaApplication.class, args);
+	}
+
+}
