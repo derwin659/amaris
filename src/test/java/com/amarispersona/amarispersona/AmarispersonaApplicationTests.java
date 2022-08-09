6a74b1ae8c4c39a7b6fbf9ea48f5bde6662d74c6
Derwin Nieves Martinez <dnieves@nisum.com>
2022-08-09 13:35:03 -0400

    primer commit

---
+++ src/test/java/com/amarispersona/amarispersona/AmarispersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarispersona.amarispersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmarispersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
